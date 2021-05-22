b87dc4e82eb5ebe8947853090a921ffbc3dc0b85
kodai1997 <takeda91@icloud.com>
2021-05-22 12:25:10 +0900

    商品出品機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,10 +21,14 @@ class Item < ApplicationRecord
     validates :image
    end
 
+   with_options  numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_days_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+   end
+
    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-   validates :category_id, numericality: { other_than: 1 }
-   validates :status_id, numericality: { other_than: 1 }
-   validates :shipping_cost_id, numericality: { other_than: 1 }
-   validates :prefecture_id, numericality: { other_than: 1 }
-   validates :shipping_days_id, numericality: { other_than: 1 }
+   
 end
