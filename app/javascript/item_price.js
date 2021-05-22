46e6945d39969db05f232e87f0f80b55cebb55c7
kodai1997 <takeda91@icloud.com>
2021-05-22 12:50:39 +0900

    商品出品機能修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,8 +3,8 @@ const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
   const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = (Math.floor(inputValue * 1.1)); 
+  addTaxDom.innerHTML = (Math.floor(inputValue * 0.1)); 
   const addProfitDom = document.getElementById("profit");
-  addProfitDom.innerHTML = (Math.floor(inputValue)); 
+  addProfitDom.innerHTML = (Math.floor(inputValue * 0.9)); 
 })
 });
